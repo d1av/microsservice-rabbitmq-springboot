9508ed50ef3500dd383205ffd4074477c9c0bb6f
d1av <davi4alves@gmail.com>
2023-01-17 00:06:17 -0300

    feat: add publisher

---
+++ order-service/src/main/java/net/java/orderservice/controller/OrderController.java
@@ -0,0 +1,39 @@
+package net.java.orderservice.controller;
+
+import net.java.orderservice.dto.Order;
+import net.java.orderservice.dto.OrderEvent;
+import net.java.orderservice.publisher.OrderProducer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity placeOrder(@RequestBody Order order) {
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent event = new OrderEvent();
+        event.setStatus("PENDING");
+        event.setMessage("Order is in pending status");
+        event.setOrder(order);
+
+        orderProducer.sendMessage(event);
+
+        return new ResponseEntity<>("Order sent to the RabbitMQ ..", HttpStatus.OK);
+    }
+}
--- order-service/src/main/java/net/java/orderservice/publisher/OrderProducer.java
+++ order-service/src/main/java/net/java/orderservice/publisher/OrderProducer.java
@@ -1,8 +1,31 @@
 package net.java.orderservice.publisher;
 
+import net.java.orderservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderProducer {
-    
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    private Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public OrderProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Order event sent to RabbitMQ => %s", orderEvent.toString()));
+        rabbitTemplate.convertAndSend(exchange, orderRoutingKey, orderEvent);
+    }
 }
