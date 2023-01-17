7f927cc5256e3dbf6664b78e7da22463e07b7915
d1av <davi4alves@gmail.com>
2023-01-16 23:33:58 -0300

    chore: folder & project structure

---
+++ order-service/src/test/java/net/java/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
