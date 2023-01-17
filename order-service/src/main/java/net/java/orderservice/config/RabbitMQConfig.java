f46406241efe54db47d3e4f40c1dcaaab93db114
d1av <davi4alves@gmail.com>
2023-01-16 23:46:54 -0300

    feat: add config class for order service

---
+++ order-service/src/main/java/net/java/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,52 @@
+package net.java.orderservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.order.name}")
+    private String orderQueue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    // spring bean for queue - order queue
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(orderQueue);
+    }
+
+    // spring bean for exchange
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+    // spring bean for binding between exchange and queue using routing key
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(orderQueue())
+                .to(exchange())
+                .with(orderRoutingKey);
+    }
+
+    // message converter
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+    // configure RabbitTemplate
+    @Bean
+    public AmqpTemplate
+
+}
